1096c027a2fc0c27a56e94ba350731c8de0cc219
furkanmavili <furkanmavili@outlook.com>
2021-06-13 21:23:43 +0300

    Initial setup

---
+++ rollup.config.js
@@ -0,0 +1,46 @@
+import typescript from "rollup-plugin-typescript2";
+import { babel } from "@rollup/plugin-babel";
+import commonjs from "@rollup/plugin-commonjs";
+import { nodeResolve } from "@rollup/plugin-node-resolve";
+import pkg from "./package.json";
+
+const createStyledComponentsTransformer = require("typescript-plugin-styled-components").default;
+const styledComponentsTransformer = createStyledComponentsTransformer();
+// to be continued
+export default {
+  input: "src/index.tsx",
+  output: [
+    {
+      file: pkg.main,
+      format: "cjs",
+      exports: "named",
+      sourcemap: true,
+      strict: false,
+    },
+  ],
+  plugins: [
+    nodeResolve(),
+    typescript({
+      objectHashIgnoreUnknownHack: true,
+      transformers: [
+        () => ({
+          before: [styledComponentsTransformer],
+        }),
+      ],
+    }),
+
+    commonjs(),
+    babel({
+      presets: ["@babel/preset-env", "@babel/preset-react"],
+      plugins: [
+        [
+          "babel-plugin-styled-components",
+          {
+            pure: true,
+          },
+        ],
+      ],
+    }),
+  ],
+  external: ["react", "react-dom"],
+};
