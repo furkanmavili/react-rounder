06de0b4ba79b00ed383763f196cc2fe999740708
furkanmavili <furkanmavili@outlook.com>
2021-06-13 21:25:49 +0300

    Add Ring Rounder

---
+++ src/components/Ring.tsx
@@ -0,0 +1,54 @@
+import * as React from 'react';
+import styled, { keyframes } from 'styled-components';
+import { rounderDefaults } from '../constants';
+import { RounderProps } from '../types';
+
+const animation = keyframes`
+    0% {
+      transform: rotate(0deg);
+    }
+    100% {
+      transform: rotate(360deg);
+    }
+`;
+
+const Container = styled.div<RounderProps>`
+  display: inline-block;
+  position: relative;
+  width: ${(props) => props.size}px;
+  height: ${(props) => props.size}px;
+
+  & div {
+    box-sizing: border-box;
+    display: block;
+    position: absolute;
+    width: 100%;
+    height: 100%;
+    border: ${(props) => (props.size ? props.size / 7 : 8)}px solid;
+    border-radius: 50%;
+    animation: ${animation} 1.2s cubic-bezier(0.5, 0, 0.5, 1) infinite;
+    border-color: ${(props) => props.color} transparent transparent transparent;
+  }
+  & div:nth-child(1) {
+    animation-delay: -0.45s;
+  }
+  & div:nth-child(2) {
+    animation-delay: -0.3s;
+  }
+  & div:nth-child(3) {
+    animation-delay: -0.15s;
+  }
+`;
+
+const Ring = ({ size = 64, color = rounderDefaults.color }: RounderProps): React.ReactNode => {
+  return (
+    <Container size={size} color={color}>
+      <div></div>
+      <div></div>
+      <div></div>
+      <div></div>
+    </Container>
+  );
+};
+
+export { Ring };
