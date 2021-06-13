2cc92d292fad64626169ded7fc9673421d1d0b76
furkanmavili <furkanmavili@outlook.com>
2021-06-13 21:24:42 +0300

    Add entry for components

---
+++ src/index.tsx
@@ -0,0 +1,9 @@
+import { Ring } from './components/Ring';
+import { Circle } from './components/Circle';
+import { DualRing } from './components/DualRing';
+import { Bars } from './components/Bars';
+import { Heart } from './components/Heart';
+import { Roller } from './components/Roller';
+import { Tail } from './components/Tail';
+
+export { Ring, Circle, DualRing, Bars, Heart, Roller, Tail };
